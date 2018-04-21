32acbc37cd54adb207de5acdf813271019c32ff2
Milan Stanojevic <milanche_show@hotmail.com>
2018-04-21 19:56:19 +0200

    added couple of clubs

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -73,19 +73,68 @@ function gallerySlider(dom, images) {
 }
 
 var gallery = document.getElementById('gallery');
-var galleryImages = [
-    "http://2k535u2tu3vk210yd2dy5v58.wpengine.netdna-cdn.com/wp-content/uploads/2016/05/Insomnia_PressPhotos_CLUB_11.jpg",
-    "https://www.badruttspalace.com/media/289552/badrutts-palace-hotel_kings-club-6.jpg",
-    "images/clubs/shake/shake1.jpg",
-    "images/clubs/shake/shake2.jpg",
-    "images/clubs/shake/shake3.jpg",
-    "images/clubs/shake/shake4.jpg",
-    "images/clubs/shake/shake5.jpg",
-    "images/clubs/shake/shake6.jpg",
-    "images/clubs/shake/shake7.jpg",
-    "images/clubs/shake/shake8.jpg",
-    "images/clubs/shake/shake9.jpg",
-    "images/clubs/shake/shake10.jpg",
-    "images/clubs/shake/shake11.jpg",
-];
+var galleryName = document.getElementById('title').textContent;
+var galleryImages = [];
+console.log(galleryName);
+switch(galleryName) {
+    case "Shake 'n' Shake":
+    galleryImages = [
+        "images/clubs/shake/shake1.jpg",
+        "images/clubs/shake/shake2.jpg",
+        "images/clubs/shake/shake3.jpg",
+        "images/clubs/shake/shake4.jpg",
+        "images/clubs/shake/shake5.jpg",
+        "images/clubs/shake/shake6.jpg",
+        "images/clubs/shake/shake7.jpg",
+        "images/clubs/shake/shake8.jpg",
+        "images/clubs/shake/shake9.jpg",
+        "images/clubs/shake/shake10.jpg",
+        "images/clubs/shake/shake11.jpg",
+    ];
+    break;
+
+    case "Lasta":
+    galleryImages = [
+        "images/clubs/lasta/lasta1.jpg",
+        "images/clubs/lasta/lasta2.jpg",
+        "images/clubs/lasta/lasta3.jpg",
+        "images/clubs/lasta/lasta4.jpg",
+        "images/clubs/lasta/lasta5.jpg",
+        "images/clubs/lasta/lasta6.jpg",
+        "images/clubs/lasta/lasta7.jpg",
+        "images/clubs/lasta/lasta8.jpg",
+        "images/clubs/lasta/lasta9.jpg",
+        "images/clubs/lasta/lasta10.jpg",
+        "images/clubs/lasta/lasta11.jpg",
+        "images/clubs/lasta/lasta12.jpg",
+        "images/clubs/lasta/lasta13.jpg",
+        "images/clubs/lasta/lasta14.jpg",
+        "images/clubs/lasta/lasta15.jpg",
+        "images/clubs/lasta/lasta16.jpg",
+        "images/clubs/lasta/lasta17.jpg",
+        "images/clubs/lasta/lasta18.jpg",
+        "images/clubs/lasta/lasta19.jpg",
+        "images/clubs/lasta/lasta20.jpg"
+    ];
+    break;
+    case "Freestyler":
+    galleryImages = [
+        "images/clubs/free/free1.jpg",
+        "images/clubs/free/free2.jpg",
+        "images/clubs/free/free3.jpg",
+        "images/clubs/free/free4.jpg",
+        "images/clubs/free/free5.jpg",
+        "images/clubs/free/free6.jpg",
+        "images/clubs/free/free7.jpg",
+        "images/clubs/free/free8.jpg",
+        "images/clubs/free/free9.jpg",
+        "images/clubs/free/free10.jpg",
+        "images/clubs/free/free11.jpg",
+        "images/clubs/free/free12.jpg",
+        "images/clubs/free/free13.jpg"
+    ];
+    break;
+}
+
+
 var galerija = new gallerySlider(gallery, galleryImages);
